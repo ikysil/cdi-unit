3e7216f49c52871508b923f637e2ae4bbdf86125
Illya Kysil <ikysil@ikysil.name>
2020-05-15 21:00:48 +0200

    Refactor discovery extensions for EasyMock and Mockito to use field discovery callbacks instead of explicit field enumeration

--- cdi-unit/src/main/java/org/jglue/cdiunit/internal/easymock/EasyMockDiscoveryExtension.java
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/easymock/EasyMockDiscoveryExtension.java
@@ -1,40 +1,33 @@
 package org.jglue.cdiunit.internal.easymock;
 
-import java.lang.reflect.Field;
-
 import org.jglue.cdiunit.internal.DiscoveryExtension;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
 public class EasyMockDiscoveryExtension implements DiscoveryExtension {
 
+	private Class<? extends Annotation> fieldAnnotation = null;
+
 	@Override
 	public void bootstrap(BootstrapDiscoveryContext bdc) {
-		bdc.discoverExtension(this::discoverCdiExtension);
-		bdc.discoverClass(this::discoverClass);
-	}
-
-	private void discoverCdiExtension(Context context) {
 		try {
-			Class.forName("org.easymock.EasyMockRunner");
-			context.extension(new EasyMockExtension(), EasyMockDiscoveryExtension.class.getName());
-		} catch (ClassNotFoundException ignore) {
+			fieldAnnotation = org.easymock.Mock.class;
+			bdc.discoverExtension(this::discoverCdiExtension);
+			bdc.discoverField(this::discoverField);
+		} catch (NoClassDefFoundError ignore) {
+			// no EasyMock
 		}
 	}
 
-	private void discoverClass(Context context, Class<?> cls) {
-		ignoreMockedBeans(context, cls);
+	private void discoverCdiExtension(Context context) {
+		context.extension(new EasyMockExtension(), EasyMockDiscoveryExtension.class.getName());
 	}
 
-	private void ignoreMockedBeans(Context context, Class<?> beanClass) {
-		// FIXME - migrate to discoverField
-		try {
-			for (Field field : beanClass.getDeclaredFields()) {
-				if (field.isAnnotationPresent(org.easymock.Mock.class)) {
-					Class<?> type = field.getType();
-					context.ignoreBean(type);
-				}
-			}
-		} catch (NoClassDefFoundError ignore) {
-			// no EasyMock
+	private void discoverField(Context context, Field field) {
+		if (field.isAnnotationPresent(fieldAnnotation)) {
+			Class<?> type = field.getType();
+			context.ignoreBean(type);
 		}
 	}
 
--- cdi-unit/src/main/java/org/jglue/cdiunit/internal/mockito/MockitoDiscoveryExtension.java
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/mockito/MockitoDiscoveryExtension.java
@@ -1,41 +1,33 @@
 package org.jglue.cdiunit.internal.mockito;
 
-import java.lang.reflect.Field;
-
 import org.jglue.cdiunit.internal.DiscoveryExtension;
-import org.mockito.Mock;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
 public class MockitoDiscoveryExtension implements DiscoveryExtension {
 
+	private Class<? extends Annotation> fieldAnnotation = null;
+
 	@Override
 	public void bootstrap(BootstrapDiscoveryContext bdc) {
-		bdc.discoverExtension(this::discoverCdiExtension);
-		bdc.discoverClass(this::discoverClass);
-	}
-
-	private void discoverCdiExtension(Context context) {
 		try {
-			Class.forName("org.mockito.Mock");
-			context.extension(new MockitoExtension(), MockitoDiscoveryExtension.class.getName());
-		} catch (ClassNotFoundException ignore) {
+			fieldAnnotation = org.mockito.Mock.class;
+			bdc.discoverExtension(this::discoverCdiExtension);
+			bdc.discoverField(this::discoverField);
+		} catch (NoClassDefFoundError ignore) {
+			// no Mockito
 		}
 	}
 
-	private void discoverClass(Context context, Class<?> cls) {
-		ignoreMockedBeans(context, cls);
+	private void discoverCdiExtension(Context context) {
+		context.extension(new MockitoExtension(), MockitoDiscoveryExtension.class.getName());
 	}
 
-	private void ignoreMockedBeans(Context context, Class<?> beanClass) {
-		// FIXME - migrate to discoverField
-		try {
-			for (Field field : beanClass.getDeclaredFields()) {
-				if (field.isAnnotationPresent(Mock.class)) {
-					Class<?> type = field.getType();
-					context.ignoreBean(type);
-				}
-			}
-		} catch (NoClassDefFoundError ignore) {
-			// no Mockito
+	private void discoverField(Context context, Field field) {
+		if (field.isAnnotationPresent(fieldAnnotation)) {
+			Class<?> type = field.getType();
+			context.ignoreBean(type);
 		}
 	}
 
